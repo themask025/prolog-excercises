4c1e30df8a4a3fe8bb883c59e4207a2866f76b54
Evelin Todorov <evelintodo@gmail.com>
2025-01-04 15:20:47 +0200

    graphs: Added more predicates for paths

--- graphs.pl
+++ graphs.pl
@@ -4,6 +4,15 @@ member_my(X, [_|T]):- member_my(X, T).
 append_my([], X, X).
 append_my([Y|L], X, [Y|S]):- append_my(L, X, S).
 
+kleene_star_relaxed(_, 0, []).
+kleene_star_relaxed(L, N, [X|S]):- N > 0,
+                                   N1 is N - 1,
+                                   kleene_star_relaxed(L, N1, S),
+                                   member_my(X, L).
+
+unique_elements([_]).
+unique_elements([X|L]):- not(member_my(X, L)), unique_elements(L). 
+
 % При подадено представяне (V, E) на граф 
 % edge(V, E, X, Y) при преудовлетворяване
 % генерира в X и Y всички краища на ребра
@@ -13,9 +22,9 @@ edge(V, E, X, Y):- member_my([X,Y], E),
                    member_my(Y, V).
 
 % ако графът е ориентиран:
-edge(V, E, X, Y):- member_my([Y, X], E),
-                   member_my(X, V),
-                   member_my(Y, V).
+% edge(V, E, X, Y):- member_my([Y, X], E),
+%                    member_my(X, V),
+%                    member_my(Y, V).
 
 is_path_pairs(V, E, P):- not((
                         append_my(_, [[X, Y], [Z,T]|_], P),
@@ -29,3 +38,25 @@ is_path(V, E, P):- not((
                         not(edge(V, E, X, Y))
                         )),
                         member(_,P).
+
+% При подадено представяне (V, E) на граф и естествено число
+% N all_paths_with_length(V, E, N, P) при преудовлетворяване
+% генерира в P всички пътища с дължина N в графа (V, E)
+
+all_paths_with_length(V, E, N, P):- kleene_star_relaxed(V, N, P), is_path(V, E, P).
+
+% При подадено представяне (V, E) на граф 
+% is_simple_path(V, E, P) дава истина
+% т.с.т.к. P е прост път в графа (V, E)
+is_simple_path(V, E, P):- is_path(V, E, P), unique_elements(P).
+
+is_simple_path1(V, E, P):- is_path(V, E, P),
+                           not((append_my(_, [X|P1], P), member_my(X, P1))).
+
+
+% При подадено представяне (V, E) на граф 
+% is_hamiltonian_path(V, E, P) дава истина
+% т.с.т.к. P е Хамилтонов път в графа (V, E)
+is_hamiltonian_path(V, E, P):- is_simple_path(V, E, P),
+                                not((member_my(X, V), not(member(X, P)))).
+
